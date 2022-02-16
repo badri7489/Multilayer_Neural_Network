ef35e5c83807b75ff27c6140cd1a03bbfe4092be
Biswanath Prasad Sharma <badrisharma333@gmail.com>
2022-02-16 13:05:10 +0530

    Inputs removed

--- activations.py
+++ activations.py
@@ -2,7 +2,7 @@ import numpy as np
 from activation import Activation
 
 class Tanh(Activation):
-    def __init__(self, activation, activation_prime):
+    def __init__(self):
         # making the tanh function
         tanh = lambda x: np.tanh(x)
 
@@ -10,4 +10,4 @@ class Tanh(Activation):
         tanh_prime = lambda x: 1 - np.tanh(x) ** 2
 
         # pass the hyperbolic and its derivative function in the super init
-        super.__init__(tanh, tanh_prime)
+        super().__init__(tanh, tanh_prime)
